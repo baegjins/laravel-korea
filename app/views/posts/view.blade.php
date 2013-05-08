51b10759ed6d1cd9ac0f66ccc98d9551fcb97727
Kaden Lee <thisiskaden@gmail.com>
2013-05-08 10:48:58 +0900

    disqus title, identifier 변수 추가

--- app/views/posts/view.blade.php
+++ app/views/posts/view.blade.php
@@ -42,7 +42,9 @@ $(function(){
     <div id="disqus_thread"></div>
     <script type="text/javascript">
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = '{{ Config::get('site.disqusShortname') }}'; // required: replace example with your forum shortname
+        var disqus_shortname = '{{ Config::get('site.disqusShortname') }}';
+        var disqus_identifier = '{{ $post->id }}';
+        var disqus_title = "{{ $header . ' – ' . $post->title }}";
 
         /* * * DON'T EDIT BELOW THIS LINE * * */
         (function() {
