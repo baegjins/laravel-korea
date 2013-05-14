0c1188d44fb7cdf4b6e6b52a9500eecc27913fb7
Kaden Lee <thisiskaden@gmail.com>
2013-05-14 13:09:42 +0900

    gravatar 수정

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -9,7 +9,7 @@
   @foreach ($users as $user)
   <li class="span3">
     <div class="thumbnail">
-      <img class="user-img img-polaroid" src="http://www.gravatar.com/avatar/{{ md5( strtolower( trim( ' . $user->email . ' ) ) ) }}?d=mm&s=60" />
+      <img class="user-img img-polaroid" src="http://www.gravatar.com/avatar/{{ md5( strtolower( trim($user->email) ) ) }}?d=mm&s=60" />
       <ul class="user-info unstyled">
         <li><h4><a class="username" href="{{ URL::to('users/' . $user->id . '/' . $user->username) }}">{{ $user->username }}</a></h4></li>
         <li>{{ $user->nickname }}</li>
