cf9faf95ce8e7d5314e6723e9719f2d3f649abd7
Kaden Lee <thisiskaden@gmail.com>
2014-03-13 22:54:03 +0900

    삭제

--- app/views/layouts/includes/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer>
-  <p>&copy; 2013 Laravel Korean User Community <a href="https://github.com/thisiskaden/laravel-korea">GitHub</a> · <a href="{{ URL::to('changelog') }}">change log</a></p>
-</footer>
